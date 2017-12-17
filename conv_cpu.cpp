fe53e56f2035fd5cd40cac40bd112dad92915b5d
Ronit Halder <ronit.codin@gmail.com>
2017-12-17 16:42:03 +0530

    Initial Commit

---
+++ conv_cpu.cpp
@@ -0,0 +1,12 @@
+#include "conv_cpu.hpp"
+
+class Convolution {
+	private:
+
+	public:
+		void forward(void *in, void  *out, ) {
+		}
+		void backward() {
+			
+		}
+};
---
+++ conv_cpu.hpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+struct conv_conf {
+	
+};
